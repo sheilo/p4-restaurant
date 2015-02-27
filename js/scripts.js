fc50370cf94acfe9240e32a3c152673433e251b1
Cass Baden <cbaden@it-rpub-mac12.eastern.ewu.edu>
2015-02-27 15:12:27 -0800

    commit-3

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,14 @@ var time = new Date();
 var current = {
   hour: time.getUTCHours(),
   minute: time.getUTCMinutes(),
-  check: time.getUTCHours() + ":" + time.getUTCMinutes(), 
-  user: ['4/6', '9/23', '10/1', '1/11', '12/24', '7/7']
+  check: time.getUTCHours() + ":" + time.getUTCMinutes()
+  hours: ['11:00', '9/23', '10/1', '1/11', '12/24', '7/7']
 };
 
 var currenttime = current.check;
 console.log(currenttime);
 
-//if ( birthday.user[3] == '1/11') {
+//if ( > current.check > ) {
 //	$('body').removeClass('open');
 //	$('body').addClass('close');
 //}
